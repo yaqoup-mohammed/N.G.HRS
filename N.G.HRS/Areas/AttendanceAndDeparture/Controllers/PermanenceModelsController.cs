23efc13ac3b86ff06716b48ca7c54064cfc8a6ce
MuhammedAlsharafi <muhammedalsharafi2001.ye@gmail.com>
2024-03-06 22:03:38 +0300

    Attendance Settings

---
+++ N.G.HRS/Areas/AttendanceAndDeparture/Controllers/PermanenceModelsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using N.G.HRS.Areas.AttendanceAndDeparture.Models;
+using N.G.HRS.Date;
+
+namespace N.G.HRS.Areas.AttendanceAndDeparture.Controllers
+{
+    [Area("AttendanceAndDeparture")]
+    public class PermanenceModelsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PermanenceModelsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AttendanceAndDeparture/PermanenceModels
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.permanenceModels.ToListAsync());
+        }
+
+        // GET: AttendanceAndDeparture/PermanenceModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var permanenceModels = await _context.permanenceModels
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (permanenceModels == null)
+            {
+                return NotFound();
+            }
+
+            return View(permanenceModels);
+        }
+
+        // GET: AttendanceAndDeparture/PermanenceModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AttendanceAndDeparture/PermanenceModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,PermanenceName,FromDate,ToDate,FlexibleWorkingHours,WorkBetweenTwoDays,FromTime,ToTime,HoursOfWorks,AddAttendanceAndDeparturePermission,AllowanceForLateAttendance,EarlyDeparturePermission,Notes")] PermanenceModels permanenceModels)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(permanenceModels);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(permanenceModels);
+        }
+
+        // GET: AttendanceAndDeparture/PermanenceModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var permanenceModels = await _context.permanenceModels.FindAsync(id);
+            if (permanenceModels == null)
+            {
+                return NotFound();
+            }
+            return View(permanenceModels);
+        }
+
+        // POST: AttendanceAndDeparture/PermanenceModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,PermanenceName,FromDate,ToDate,FlexibleWorkingHours,WorkBetweenTwoDays,FromTime,ToTime,HoursOfWorks,AddAttendanceAndDeparturePermission,AllowanceForLateAttendance,EarlyDeparturePermission,Notes")] PermanenceModels permanenceModels)
+        {
+            if (id != permanenceModels.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(permanenceModels);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PermanenceModelsExists(permanenceModels.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(permanenceModels);
+        }
+
+        // GET: AttendanceAndDeparture/PermanenceModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var permanenceModels = await _context.permanenceModels
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (permanenceModels == null)
+            {
+                return NotFound();
+            }
+
+            return View(permanenceModels);
+        }
+
+        // POST: AttendanceAndDeparture/PermanenceModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var permanenceModels = await _context.permanenceModels.FindAsync(id);
+            if (permanenceModels != null)
+            {
+                _context.permanenceModels.Remove(permanenceModels);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PermanenceModelsExists(int id)
+        {
+            return _context.permanenceModels.Any(e => e.Id == id);
+        }
+    }
+}
--- N.G.HRS/Areas/AttendanceAndDeparture/Models/PermanenceModels.cs
+++ N.G.HRS/Areas/AttendanceAndDeparture/Models/PermanenceModels.cs
@@ -15,23 +15,81 @@ namespace N.G.HRS.Areas.AttendanceAndDeparture.Models
         public string PermanenceName { get; set; }
         [Required]
         [DataType(DataType.Date)]
-        public DateOnly FromDate { get; set; }
+        public DateOnly FromDate { 
+            get { 
+            if(FromDate>ToDate)
+                {
+                    throw new Exception("يجب ان يكون تاريخ البدء اقل من تاريخ الانتهاء");
+                }
+            else if(FromDate < DateOnly.FromDateTime(DateTime.Now))
+                {
+                    throw new Exception("يجب ان لا يكون تاريخ البدء اصغر من التاريخ الحالي");
+                }
+                    return FromDate;
+            } set { } }
         [Required]
         [DataType(DataType.Date)]
-        public DateOnly ToDate { get; set; }
+        public DateOnly ToDate { 
+            get { 
+            if(FromDate > ToDate)
+                {
+                    throw new Exception("يجب ان يكون تاريخ الانتهاء اكبر من تاريخ البدء");
+                }
+                return ToDate;
+
+            } set { } }
         public bool FlexibleWorkingHours { get; set; }
-        public bool WorkBetweenTwoShifts { get; set; }
-        public bool ShiftTime { get; set; }
-        public bool Working24Hours { get; set; }
+        public bool WorkBetweenTwoDays { get; set; }
+        //public bool ShiftTime { get; set; }
+        //public bool Working24Hours { get; set; }
         [DataType(DataType.Time)]
-        public DateTime FromTime { get; set; }
+        public DateTime FromTime { 
+            get {
+            if(FromTime > ToTime)
+                {
+                    throw new Exception("يجب ان يكون وقت البدء اقل من وقت النهاية");
+                }
+            else if (FromTime.TimeOfDay < DateTime.Now.TimeOfDay)
+                {
+                    throw new Exception("يجب ان لا يكون وقت البدء اصغر من وقت الحالي");
+                }
+                    return FromTime;
+            } set { } }
         [DataType(DataType.Time)]
-        public DateTime ToTime { get; set; }
-        [Range(0, 50)]
-        public int? HoursOfWorks { get; set; }
+        public DateTime ToTime { 
+            get {
+            if(FromTime > ToTime)
+                {
+                    throw new Exception("يجب ان يكون وقت النهاية اكبر من وقت البدء");
+                }
+            return ToTime;
+            } set { } }
+        [Range(0, 24)]
+        public double? HoursOfWorks { 
+            get {
+                try
+                {
+                    if (FlexibleWorkingHours)
+                    {
+                        return CalculateHourOfWork();
+                    }
+                    else if(WorkBetweenTwoDays) 
+                    {
+                    return CalculateHourOfWorkBetweenDays();
+                    }
+                    else 
+                    {
+                        return CalculateHourOfWork();
+                    }
+                }
+                catch
+                {
+                    throw new Exception("يجب ان تكون تاريخ البدء اقل من تاريخ الانتهاء");
+                }
+            } set { } }
         public bool AddAttendanceAndDeparturePermission { get; set; }
-        public int? AllowanceForLateAttendance { get; set; }
-        public int? EarlyDeparturePermission { get; set; }
+        public double? AllowanceForLateAttendance { get; set; }//سماحية حظور متأخر
+        public double? EarlyDeparturePermission { get; set; }//سماحية مغادرة مبكرة
         [StringLength(255)]
         public string? Notes {  get; set; }
         //==========================================
@@ -45,8 +103,52 @@ namespace N.G.HRS.Areas.AttendanceAndDeparture.Models
         public List<AdjustingTime>? AdjustingTimeList { get; set; }
         //=
 
+        //public  double CalculateHourOfWorkBetweenDays()
+        //{
+        //    DateOnly nextDay = FromDate.AddDays(1);
+        //    DateTime firstDate = new DateTime(FromDate.Year, FromDate.Month, FromDate.Day, FromTime.Hour, FromTime.Minute, FromTime.Second);
+        //    DateTime secondDate = new DateTime(nextDay.Year,nextDay.Month,nextDay.Day, ToTime.Hour, ToTime.Minute, ToTime.Second);
 
+        //    TimeSpan timeSpan = secondDate - firstDate;
 
+        //    double totalHours = timeSpan.Days * 24 + timeSpan.Hours;
+
+        //    return totalHours;
+        //}
+        public  double CalculateHourOfWorkBetweenDays()
+        {
+            DateOnly nextDay = FromDate.AddDays(1);
+            DateTime firstDate = new DateTime(FromDate.Year, FromDate.Month, FromDate.Day, FromTime.Hour, FromTime.Minute, FromTime.Second);
+            DateTime secondDate = new DateTime(nextDay.Year,nextDay.Month,nextDay.Day, ToTime.Hour, ToTime.Minute, ToTime.Second);
+
+            if(AddAttendanceAndDeparturePermission)
+            {
+                TimeSpan withPermission = secondDate.AddMinutes(-EarlyDeparturePermission??0) - firstDate.AddMinutes(AllowanceForLateAttendance??0);
+
+                return withPermission.TotalHours;
+            }
+            TimeSpan timeSpan = secondDate - firstDate;
+
+            double totalHours = timeSpan.Hours;
+
+            return totalHours;
+        }
+        public double CalculateHourOfWork()
+        {
+            DateTime firstTime = new DateTime(FromDate.Year, FromDate.Month, FromDate.Day, FromTime.Hour, FromTime.Minute, FromTime.Second);
+            DateTime secondTime = new DateTime(FromDate.Year, FromDate.Month, FromDate.Day, ToTime.Hour, ToTime.Minute, ToTime.Second);
+            if (AddAttendanceAndDeparturePermission)
+            {
+                TimeSpan withPermission = secondTime.AddMinutes(-EarlyDeparturePermission ?? 0) - firstTime.AddMinutes(AllowanceForLateAttendance ?? 0);
+
+                return withPermission.TotalHours;
+            }
+            TimeSpan timeSpan = secondTime - firstTime;
+
+            int totalHours = timeSpan.Hours;
+
+            return totalHours;
+        }
 
 
     }
