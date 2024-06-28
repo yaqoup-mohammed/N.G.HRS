7f00cd58f263f3c2ed1576133b34c7395363caed
Muhammed Alsharafi <106452735+MuhammedAlsharafi@users.noreply.github.com>
2024-06-28 22:59:57 +0300

    Revert "ؤخةهف"

--- N.G.HRS/Repository/File_Upload/IFileUploadService (2).cs
+++
@@ -1,8 +0,0 @@
-﻿namespace N.G.HRS.Repository.File_Upload
-{
-    public interface IFileUploadService
-    {
-        Task<string> UploadFileAsync(IFormFile file, string baseUploadPath);
-
-    }
-}
