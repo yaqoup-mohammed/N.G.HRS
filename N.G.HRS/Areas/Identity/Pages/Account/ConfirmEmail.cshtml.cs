02de978bff091945992b0b780ba0cff2d7f93654
yaqoup-mohammed <149320740+yaqoup-mohammed@users.noreply.github.com>
2024-07-16 01:09:33 +0300

    ص1

--- N.G.HRS/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ N.G.HRS/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -22,7 +22,7 @@ namespace N.G.HRS.Areas.Identity.Pages.Account
         {
             _userManager = userManager;
         }
-
+            
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
--- N.G.HRS/Program.cs
+++ N.G.HRS/Program.cs
@@ -75,7 +75,7 @@ using (var scope = app.Services.CreateScope())
     {
         var adminUser = new IdentityUser { UserName = "adminn@example.com", Email = "adminn@example.com" };
         var result = await userManager.CreateAsync(adminUser, "Yaqoup@1234");
-        if (result.Succeeded)
+        if (result.Succeeded) 
         {
             // إضافة أدوار "Admin" للمستخدم
             var roles = new[] { "Add", "Edit", "View", "Admin", "Delete", "Details", "MalePhoto", "FemalePhoto", "Profile" };
