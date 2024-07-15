6b366d6eea86be7eadcc0bed90f8793dbee654f3
Muhammed Alsharafi <muhammedalsharafi2001.ye@gmail.com>
2024-07-15 21:03:17 +0300

    comit

--- N.G.HRS/Program.cs
+++ N.G.HRS/Program.cs
@@ -69,6 +69,7 @@ using (var scope = app.Services.CreateScope())
     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
     var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
+
     var existingUser = await userManager.FindByNameAsync("adminn@example.com");
 
     if (existingUser == null)
